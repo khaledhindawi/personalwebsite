e93b57dfaea5a7cab650c75cfc06716ba471c327
khaledhindawi <k.hindawi2001@gmail.com>
2017-07-20 00:07:19 -0700

    added contact page

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 import random
 app= Flask(__name__)
 @app.route("/")
 
 def load_page():
-	fortune=["f","d","r","w","y"]
-	fortune1=["er","yt","gh","tt","df"]
-	return render_template ("index.html",letter=random.choice(fortune),twolettera=random.choice(fortune1))
+	fortune=["chocoleta is the life","If you hate chocolata close this web","cocolata is the tast of love","you have to eat chocolate every time,and forever","welcome in our team of cocoleta addicts"]
+	return render_template ("index.html",qouts=random.choice(fortune),)
 	# return render_template ("index.html")
 
 
@@ -15,6 +14,23 @@ def load_page():
 @app.route("/aboutme")
 def aboutme():
 	return render_template ("aboutme.html")
+@app.route("/contact")
+def contact():
+	return render_template ("contact.html")
+
+# @app.route("/form/")
+# def form():
+	# return render_template ("input_form.html")
+
+@app.route("/form_response",methods=["POST"])
+def form_res():
+	user_firstname=request.form["firstname"]
+	user_lastname=request.form["lastname"]
+	user_Messges=request.form["messege"]
+	user_gender=request.form["gender"]
+	return render_template ("data.html",thefirstname=user_firstname,thelastname=user_lastname,themessege=user_Messges,thegender=user_gender)
+
+
 
 
 
